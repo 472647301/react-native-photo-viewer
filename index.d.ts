2f079357ead5f87c292d2dbdb533c35bc2c33461
秃尾巴的猫 <472647301@qq.com>
2022-05-24 17:50:43 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,8 @@ declare module "@byron-react-native/photo-viewer" {
   class PhotoView {
     static show(params: {
       list: string[];
-      index: number;
-      onChange: (index: number) => void;
+      index?: number;
+      onChange?: (index: number) => void;
     }): void;
   }
   export default PhotoView;
